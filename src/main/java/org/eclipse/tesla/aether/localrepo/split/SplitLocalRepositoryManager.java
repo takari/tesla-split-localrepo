4b23a5629922f2662bcc7f524e76914945e5743a
Matt Benson <gudnabrsam@gmail.com>
2021-08-19 14:35:05 -0500

    update copyright years

--- src/main/java/org/eclipse/tesla/aether/localrepo/split/SplitLocalRepositoryManager.java
+++ src/main/java/org/eclipse/tesla/aether/localrepo/split/SplitLocalRepositoryManager.java
@@ -1,7 +1,7 @@
 package org.eclipse.tesla.aether.localrepo.split;
 
 /*******************************************************************************
- * Copyright (c) 2011 Sonatype, Inc.
+ * Copyright (c) 2011, 2021 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
